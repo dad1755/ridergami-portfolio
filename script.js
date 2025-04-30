e5aa5f5a0a6eba1d8b0432f6c7bd7a3e4acbe0ad
ridergami <ridergami@ridergami.app.my>
2025-04-30 15:02:32 +0800

    Initial commit: Add portfolio webpage

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('cta-btn').addEventListener('click', function() {
+    alert('Thanks for clicking! You will be redirected soon...');
+    // You can redirect to a new page here if needed:
+    // window.location.href = "https://www.yourwebsite.com";
+  });
+  
